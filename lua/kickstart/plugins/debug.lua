e46625362bf83639244fd733c52760a493de596e
k.lobastov <lobastov.k@yandex.ru>
2024-09-11 15:44:22 +0700

    Add PHP debug tools. Highlitin DAP via treesitter on xdebug not working. Virtual text on debugging maybe should configure, but current still work

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -1,15 +1,5 @@
--- debug.lua
---
--- Shows how to use the DAP plugin to debug your code.
---
--- Primarily focused on configuring the debugger for Go, but can
--- be extended to other languages as well. That's why it's called
--- kickstart.nvim and not kitchen-sink.nvim ;)
-
 return {
-  -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
-  -- NOTE: And you can specify dependencies as well
   dependencies = {
     -- Creates a beautiful debugger UI
     'rcarriga/nvim-dap-ui',
@@ -21,85 +11,299 @@ return {
     'williamboman/mason.nvim',
     'jay-babu/mason-nvim-dap.nvim',
 
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
+    -- nvim-dap features
+    'theHamsta/nvim-dap-virtual-text',
   },
   keys = function(_, keys)
     local dap = require 'dap'
     local dapui = require 'dapui'
     return {
-      -- Basic debugging keymaps, feel free to change to your liking!
-      { '<F5>', dap.continue, desc = 'Debug: Start/Continue' },
-      { '<F1>', dap.step_into, desc = 'Debug: Step Into' },
-      { '<F2>', dap.step_over, desc = 'Debug: Step Over' },
-      { '<F3>', dap.step_out, desc = 'Debug: Step Out' },
-      { '<leader>b', dap.toggle_breakpoint, desc = 'Debug: Toggle Breakpoint' },
       {
-        '<leader>B',
+        '<leader>db',
         function()
           dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
         end,
-        desc = 'Debug: Set Breakpoint',
+        desc = 'Breakpoint Condition',
       },
-      -- Toggle to see last session result. Without this, you can't see session output in case of unhandled exception.
-      { '<F7>', dapui.toggle, desc = 'Debug: See last session result.' },
+      {
+        '<leader>dd',
+        function()
+          dap.toggle_breakpoint()
+        end,
+        desc = 'Toggle Breakpoint',
+      },
+      {
+        '<F5>',
+        function()
+          dap.continue()
+        end,
+        desc = 'Start/Continue',
+      },
+      {
+        '<F8>',
+        function()
+          dap.run_to_cursor()
+        end,
+        desc = 'Run to Cursor',
+      },
+      {
+        '<F1>',
+        function()
+          dap.step_into()
+        end,
+        desc = 'Step Into',
+      },
+      {
+        '<F10>',
+        function()
+          dap.run_last()
+        end,
+        desc = 'Run Last',
+      },
+      {
+        '<F2>',
+        function()
+          dap.step_out()
+        end,
+        desc = 'Step Out',
+      },
+      {
+        '<F3>',
+        function()
+          dap.step_over()
+        end,
+        desc = 'Step Over',
+      },
+      {
+        '<leader>dr',
+        function()
+          dap.repl.toggle()
+        end,
+        desc = 'Toggle REPL',
+      },
+      {
+        '<F7>',
+        function()
+          dapui.toggle()
+        end,
+        desc = 'See last session result',
+      },
+      {
+        '<F4>',
+        function()
+          require('dap.ui.widgets').hover()
+        end,
+        desc = 'Widgets hover',
+      },
       unpack(keys),
     }
+  end,
+  init = function()
+    vim.api.nvim_create_autocmd({ 'FileType' }, {
+      pattern = 'dap-float',
+      callback = function()
+        vim.keymap.set('n', '<ESC>', '<cmd>close<cr>', { buffer = true, silent = true })
+      end,
+    })
   end,
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
-
+    require('nvim-dap-virtual-text').setup {
+      enabled = true,
+    }
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_installation = true,
+      handlers = {
+        function(config)
+          -- all sources with no handler get passed here
 
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
-      handlers = {},
+          -- Keep original functionality
+          require('mason-nvim-dap').default_setup(config)
+        end,
+        php = function(config)
+          config.adapters = {
+            type = 'executable',
+            command = 'node',
+            args = { vim.fn.stdpath 'data' .. '/mason/packages/php-debug-adapter/extension/out/phpDebug.js' },
+          }
+          config.configurations = {
+            {
+              type = 'php',
+              prepl_lang = 'php',
+              request = 'launch',
+              name = 'Debug current script locally',
+              port = 9003,
+              hostname = '0.0.0.0',
+              log = false,
+              program = '${file}',
+              cwd = '${fileDirname}',
+              runtimeArgs = { '-dxdebug.start_with_request=yes' },
+              env = {
+                XDEBUG_MODE = 'debug,develop',
+                XDEBUG_CONFIG = 'client_port=${port}',
+              },
+              pathMappings = {
+                ['${workspaceFolder}'] = '${workspaceFolder}',
+              },
+            },
+            {
+              type = 'php',
+              prepl_lang = 'php',
+              request = 'launch',
+              name = 'Debug current script with args locally',
+              port = 9003,
+              hostname = '0.0.0.0',
+              log = false,
+              program = '${file}',
+              cwd = '${fileDirname}',
+              runtimeArgs = { '-dxdebug.start_with_request=yes' },
+              env = {
+                XDEBUG_MODE = 'debug,develop',
+                XDEBUG_CONFIG = 'client_port=${port}',
+              },
+              args = function()
+                local args = type(config.args) == 'function' and (config.args() or {}) or config.args or {}
+                args = type(args) == 'table' and args or {}
+                config = vim.deepcopy(config)
+                ---@cast args string[]
+                local new_args = vim.fn.input('Run with args: ', table.concat(args, ' ')) --[[@as string]]
+                return vim.split(vim.fn.expand(new_args) --[[@as string]], ' ')
+              end,
+              pathMappings = {
+                ['${workspaceFolder}'] = '${workspaceFolder}',
+              },
+            },
+            {
+              type = 'php',
+              prepl_lang = 'php',
+              request = 'launch',
+              name = 'Run current script locally',
+              port = 9003,
+              cwd = '${fileDirname}',
+              program = '${file}',
+              runtimeExecutable = 'php',
+              pathMappings = {
+                ['${workspaceFolder}'] = '${workspaceFolder}',
+              },
+            },
+            {
+              type = 'php',
+              prepl_lang = 'php',
+              request = 'launch',
+              name = 'Run current script with args locally',
+              port = 9003,
+              cwd = '${fileDirname}',
+              program = '${file}',
+              runtimeExecutable = 'php',
+              args = function()
+                local args = type(config.args) == 'function' and (config.args() or {}) or config.args or {}
+                args = type(args) == 'table' and args or {}
+                config = vim.deepcopy(config)
+                ---@cast args string[]
+                local new_args = vim.fn.input('Run with args: ', table.concat(args, ' ')) --[[@as string]]
+                return vim.split(vim.fn.expand(new_args) --[[@as string]], ' ')
+              end,
+              pathMappings = {
+                ['${workspaceFolder}'] = '${workspaceFolder}',
+              },
+            },
+          }
+          require('mason-nvim-dap').default_setup(config)
+        end,
+      },
 
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        'php',
       },
     }
 
-    -- Dap UI setup
-    -- For more information, see |:help nvim-dap-ui|
     dapui.setup {
-      -- Set icons to characters that are more likely to work in every terminal.
-      --    Feel free to remove or use ones that you like more! :)
-      --    Don't feel like these are good choices.
+      force_buffers = true,
       icons = { expanded = '▾', collapsed = '▸', current_frame = '*' },
+      mappings = {
+        -- Use a table to apply multiple mappings
+        expand = { '<CR>', '<2-LeftMouse>' },
+        open = 'o',
+        remove = 'd',
+        edit = 'e',
+        repl = 'r',
+        toggle = 't',
+      },
+      expand_lines = vim.fn.has 'nvim-0.7' == 1,
       controls = {
+        -- Requires Neovim nightly (or 0.8 when released)
+        enabled = true,
+        -- Display controls in this element
+        element = 'repl',
         icons = {
-          pause = '⏸',
-          play = '▶',
-          step_into = '⏎',
-          step_over = '⏭',
-          step_out = '⏮',
-          step_back = 'b',
-          run_last = '▶▶',
-          terminate = '⏹',
-          disconnect = '⏏',
+          pause = '',
+          play = '',
+          step_into = '↘️',
+          step_over = '➡️',
+          step_out = '↖️',
+          step_back = '',
+          run_last = '↻',
+          terminate = '□',
         },
+      },
+      element_mappings = {
+        scopes = {
+          edit = 'e',
+          repl = 'r',
+        },
+        watches = {
+          edit = 'e',
+          repl = 'r',
+        },
+        stacks = {
+          open = 'o',
+        },
+        breakpoints = {
+          open = 'o',
+          toggle = 't',
+        },
+      },
+      layouts = {
+        {
+          elements = {
+            'scopes',
+            'stacks',
+            'watches',
+          },
+          size = 0.2, -- 40 columns
+          position = 'left',
+        },
+        {
+          elements = {
+            'repl',
+            'breakpoints',
+          },
+          size = 0.3,
+          position = 'bottom',
+        },
+      },
+      floating = {
+        max_height = nil,
+        max_width = nil,
+        border = 'single', -- Border style. Can be "single", "double" or "rounded"
+        mappings = {
+          close = { 'q', '<Esc>' },
+        },
+      },
+      windows = { indent = 1 },
+      render = {
+        indent = 1,
+        max_type_length = nil, -- Can be integer or nil.
+        max_value_lines = 100, -- Can be integer or nil.
       },
     }
 
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
   end,
 }
--- lua/kickstart/plugins/treesitter.lua
+++ lua/kickstart/plugins/treesitter.lua
@@ -2,27 +2,49 @@ return {
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
-    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    event = { 'BufReadPost', 'BufNewFile' },
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = {
+        'bash',
+        'dap_repl',
+        'diff',
+        'lua',
+        'luadoc',
+        'markdown',
+        'markdown_inline',
+        'php',
+        'query',
+        'regex',
+        'sql',
+        'tmux',
+        'vim',
+        'vimdoc',
+      },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
         enable = true,
-        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
-        --  If you are experiencing weird indenting issues, add the language to
-        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        disable = function(lang, buf)
+          local max_filesize = 100 * 1024 -- 100 KB
+          local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
+          if ok and stats and stats.size > max_filesize then
+            return true
+          end
+        end,
+        additional_vim_regex_highlighting = false,
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = {
+        enable = true,
+        disable = {},
+      },
+      matchup = {
+        enable = true,
+      },
     },
-    -- There are additional nvim-treesitter modules that you can use to interact
-    -- with nvim-treesitter. You should go explore a few and see what interests you:
-    --
-    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+    config = function(_, opts)
+      require('nvim-dap-repl-highlights').setup()
+      require('nvim-treesitter.configs').setup(opts)
+    end,
   },
 }
 -- vim: ts=2 sts=2 sw=2 et
