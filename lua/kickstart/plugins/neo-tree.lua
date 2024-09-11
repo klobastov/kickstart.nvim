b31a42bfa0624fbd0e91bf7223e2a639d5af3ab6
k.lobastov <lobastov.k@yandex.ru>
2024-09-11 15:46:36 +0700

    Neotree config addition

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -21,6 +21,11 @@ return {
           ['<space>'] = 'toggle_preview',
         },
       },
+      follow_current_file = {
+        enabled = true, -- This will find and focus the file in the active buffer every time
+        --               -- the current file is changed while the tree is open.
+        leave_dirs_open = true, -- `false` closes auto expanded dirs, such as with `:Neotree reveal`
+      },
       filtered_items = {
         visible = true,
         show_hidden_count = true,
