4f820b3166278d1b06061638c89c5398c90e9314
k.lobastov <lobastov.k@yandex.ru>
2024-09-25 00:37:32 +0700

    Update options after isntall plugins

--- lua/options.lua
+++ lua/options.lua
@@ -1,74 +1,89 @@
--- [[ Setting options ]]
--- See `:help vim.opt`
--- NOTE: You can change these options as you wish!
---  For more options, you can see `:help option-list`
+-- [[ Setting oions ]]
+-- See `:help vim.o`
+-- NOTE: You can change these oions as you wish!
+--  For more oions, you can see `:help oion-list`
 
--- Make line numbers default
-vim.opt.number = true
+-- Providers
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
-vim.opt.mouse = 'a'
+vim.o.mouse = 'a'
 
 -- Don't show the mode, since it's already in the status line
-vim.opt.showmode = false
+vim.o.showmode = false
 
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
---  Remove this option if you want your OS clipboard to remain independent.
+--  Remove this oion if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
 vim.schedule(function()
-  vim.opt.clipboard = 'unnamedplus'
+  vim.o.clipboard = 'unnamedplus'
 end)
 
 -- Enable break indent
-vim.opt.breakindent = true
+vim.o.breakindent = true
+vim.o.linebreak = true
 
 -- Save undo history
-vim.opt.undofile = true
+vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
+vim.o.ignorecase = true
+vim.o.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.o.signcolumn = 'yes'
 
 -- Decrease update time
-vim.opt.updatetime = 250
+vim.o.updatetime = 250
 
 -- Decrease mapped sequence wait time
 -- Displays which-key popup sooner
-vim.opt.timeoutlen = 300
+vim.o.timeoutlen = 300
 
 -- Configure how new splits should be opened
-vim.opt.splitright = true
-vim.opt.splitbelow = true
+vim.o.splitright = true
+vim.o.splitbelow = true
 
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
-
 -- Preview substitutions live, as you type!
-vim.opt.inccommand = 'split'
+vim.o.inccommand = 'split'
 
 -- Show which line your cursor is on
-vim.opt.cursorline = true
+vim.o.cursorline = true
 
 -- Minimal number of screen lines to keep above and below the cursor.
-vim.opt.scrolloff = 10
+vim.o.scrolloff = 10
 
 -- CWD in neo-tree
-vim.opt.autochdir = true
+vim.o.autochdir = true
 
 -- 24-bit colors
-vim.opt.termguicolors = true
+vim.o.termguicolors = true
 
 -- theme scheme
-vim.opt.background = 'light'
+vim.o.background = 'light'
+
+-- Status line of the current window
+vim.o.showcmdloc = 'statusline'
+
+-- Show the line and column number of the cursor position, separated by comma
+vim.o.ruler = false
+
+-- lualine and incline options
+vim.o.laststatus = 3
+vim.o.statusline = '%{""}'
+vim.o.title = true
+vim.o.titlestring = '%f'
 
 -- vim: ts=2 sts=2 sw=2 et
